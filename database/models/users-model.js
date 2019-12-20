0b5ebbe0856ddc6e47e624ea59f3c11411a92392
Mark Halls <mark.halls@gmail.com>
2019-12-20 11:49:36 -0700

    Make add return the user object added

--- database/models/users-model.js
+++ database/models/users-model.js
@@ -9,7 +9,13 @@ const find = username => {
 };
 
 const add = user => {
-  return db(`users`).insert(user, "id");
+  return db(`users`)
+    .insert(user, "id")
+    .then(([id]) =>
+      db(`users`)
+        .where({ id })
+        .first()
+    );
 };
 
 const remove = id => {
